66fbc418fd8748e32e31b1730eccfbdb755d4dec
ronn304 <brankosh1234@gmail.com>
2022-03-06 20:59:41 +0300

    create StudentForm component for input values

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AllStudentsComponent } from './all-students/all-students.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { StudentFormComponent } from './student-form/student-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AllStudentsComponent,
-    NavBarComponent
+    NavBarComponent,
+    StudentFormComponent
   ],
   imports: [
     BrowserModule,
